ae49ae79c2b0e4779f2cbb25e8154405b5a8d8c6
Vladimir Vuckovic <vidrak97@gmail.com>
2025-02-05 12:34:31 +0100

    feat: create post endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express, { query } from "express";
+import express from "express";
 import { generateCredentialResponse } from "./services/generateCredentialResponse";
+import { generateCustomCredential } from "./services/generateCustomCredentialResponse";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -8,6 +9,21 @@ app.get("/generateCredential", async (_, res) => {
     const result = await generateCredentialResponse("mock_user");
     res.send(result);
 });
+
+app.use(express.json());
+
+app.post("/generateCustomCredential", async (req, res) => {
+    try {
+        const diplomaData = req.body;
+
+        const result = await generateCustomCredential(diplomaData);
+        res.json(result);
+    } catch (error: any) {
+        res.status(500).json({ error: error.message || "Failed to generate credential" });
+    }
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
