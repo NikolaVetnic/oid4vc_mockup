53360f27a80f9ba3ea5cf06c888b950f8e650ac2
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-02-05 15:41:19 +0100

    chore: add comment

--- data/holderPublicKeyJwk.ts
+++ data/holderPublicKeyJwk.ts
@@ -3,6 +3,6 @@ export const holderPublicKeyJwk = {
     ext: true,
     key_ops: ["verify"],
     kty: "EC",
-    x: "b4PmXd8058bBtcA4uxgASyNcbmqM7o6OS94XNpRzIvc",
+    x: "b4PmXd8058bBtcA4uxgASyNcbmqM7o6OS94XNpRzIvc", // Use one of these to validate that JWT is valid and was really signed by the one who claims it.
     y: "tcgGd73JF6T47IzWR6gj-ortTqQvMpBtTyn-6lMSrL8",
 };
